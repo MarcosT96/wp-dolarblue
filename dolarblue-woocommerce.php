9eeecb2aebd3cf1949e8e7dfc7e0483f5ab3d9fe
Marcos Tomassi - Grupo MET <info@grupo-met.com>
2023-08-03 10:37:54 -0300

    Primeros cambios en Dolar Blue WooCommerce

---
+++ dolarblue-woocommerce.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: DolarBlue WooCommerce
+Plugin URI: https://www.grupo-met.com
+Description: Este plugin convierte los precios a ARS utilizando el valor del dólar blue obtenido de una API externa.
+Version: 1.0
+Author: Marcos Tomassi - Grupo MET
+Author URI: https://www.grupo-met.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: dolarblue-woocommerce
+Domain Path: /languages
+*/
